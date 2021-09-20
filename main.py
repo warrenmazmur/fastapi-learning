39c7e40db76714f30b5c27543248849a3c28dad3
warrenmazmur <warrenmazmur@gmail.com>
2021-09-20 11:18:49 +0700

    implement path parameters

--- main.py
+++ main.py
@@ -4,10 +4,20 @@ app = FastAPI()
 
 @app.get('/')
 def index():
-    return {'message' : {'hello' : 'world'}}
+    return {'message' : {'hello': 'world'}}
 
 @app.get('/about')
 def about():
-    return {'data' : 'about page'}
+    return {'data': 'about page'}
 
-    
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data': 'this is unpublished page'}
+
+@app.get('/blog/{id}')
+def blog(id: int):
+    return {'data': id}
+
+@app.get('/blog/{id}/comments')
+def comments(id: int):
+    return {'comments': {'hi', 'hello'}}
