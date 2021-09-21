f7551ff2820495b0f98f2fc19577ca3df8c35745
warrenmazmur <warrenmazmur@gmail.com>
2021-09-21 13:55:59 +0700

    implement response code and exceptions

--- blog/main.py
+++ blog/main.py
@@ -1,5 +1,4 @@
-from typing import final
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, status, Response, HTTPException
 from . import schemas, models
 from .database import SessionLocal, engine
 from sqlalchemy.orm import Session
@@ -17,7 +16,7 @@ def get_db():
 
 
 # create new blog and insert to db
-@app.post('/blog')
+@app.post('/blog', status_code=status.HTTP_201_CREATED)
 def create(request: schemas.Blog, db: Session = Depends(get_db)):
     new_blog = models.Blog(title=request.title, body=request.body)
     db.add(new_blog)
@@ -32,7 +31,12 @@ def get_all(db: Session = Depends(get_db)):
     return blogs
 
 # get specific blog with id == id
-@app.get('/blog/{id}')
-def get_blog(id, db: Session = Depends(get_db)):
+@app.get('/blog/{id}', status_code = 200)
+def get_blog(id, response: Response, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                            detail=f'Blog with id {id} is not available.')
+        # response.status_code = 404
+        # return {'detail': f'Blog with id {id} is not available.'}
     return blog
