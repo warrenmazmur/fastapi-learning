f911dfc1f6ee61b1fd8cbb1da835f807fd6f4284
warrenmazmur <warrenmazmur@gmail.com>
2021-09-21 13:19:33 +0700

    implement database queries

--- blog/main.py
+++ blog/main.py
@@ -15,6 +15,8 @@ def get_db():
     finally:
         db.close()
 
+
+# create new blog and insert to db
 @app.post('/blog')
 def create(request: schemas.Blog, db: Session = Depends(get_db)):
     new_blog = models.Blog(title=request.title, body=request.body)
@@ -22,3 +24,15 @@ def create(request: schemas.Blog, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(new_blog)
     return new_blog
+
+# get all blog in db
+@app.get('/blog')
+def get_all(db: Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    return blogs
+
+# get specific blog with id == id
+@app.get('/blog/{id}')
+def get_blog(id, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    return blog
