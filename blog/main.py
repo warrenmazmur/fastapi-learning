d96fa870a5ddd2feaf619072f5fddb3a3d317cef
warrenmazmur <warrenmazmur@gmail.com>
2021-09-22 08:26:27 +0700

    implement all CRUD

--- blog/main.py
+++ blog/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends, status, Response, HTTPException
+from fastapi import FastAPI, Depends, status, HTTPException
 from . import schemas, models
 from .database import SessionLocal, engine
 from sqlalchemy.orm import Session
@@ -17,12 +17,13 @@ def get_db():
 
 # create new blog and insert to db
 @app.post('/blog', status_code=status.HTTP_201_CREATED)
-def create(request: schemas.Blog, db: Session = Depends(get_db)):
+def create_blog(request: schemas.Blog, db: Session = Depends(get_db)):
     new_blog = models.Blog(title=request.title, body=request.body)
     db.add(new_blog)
     db.commit()
     db.refresh(new_blog)
     return new_blog
+
 
 # get all blog in db
 @app.get('/blog')
@@ -30,13 +31,28 @@ def get_all(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
+
 # get specific blog with id == id
-@app.get('/blog/{id}', status_code = 200)
-def get_blog(id, response: Response, db: Session = Depends(get_db)):
+@app.get('/blog/{id}', status_code = status.HTTP_200_OK)
+def get_blog(id: int, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
     if not blog:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
                             detail=f'Blog with id {id} is not available.')
-        # response.status_code = 404
-        # return {'detail': f'Blog with id {id} is not available.'}
     return blog
+
+
+# delete specific blog with id == id
+@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def delete_blog(id: int, db: Session = Depends(get_db)):
+    db.query(models.Blog).filter(models.Blog.id == id).delete(synchronize_session=False)
+    db.commit()
+    return {'detail': f'Blog with id {id} has been deleted.'}
+
+
+# update blogs with id == id
+@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
+def update_blog(id: int, request: schemas.Blog, db: Session = Depends(get_db)):
+    db.query(models.Blog).filter(models.Blog.id == id).update({'title': request.title, 'body': request.body})
+    db.commit()
+    return {'detail': f'Blog with id {id} has been updated'}
